90f778545bbb9fee324f32ace8a1e292c0e48d15
Luis Altuve <luis2ra@gmail.com>
2020-08-24 23:17:28 -0400

    fase 3 de la API-Products. Se agrega la app rest_framework a la lista de apps

--- drf_03_products/catalog/views.py
+++ drf_03_products/catalog/views.py
@@ -1,12 +1,9 @@
-from rest_framework.generics import ListCreateAPIView
+from rest_framework.generics import ListAPIView
 from rest_framework.response import Response
 from .models import Product
 from .serializers import ProductSerializer
 
-'''
-Probamos el uso de ListCreateAPIView en vez de ListAPIView
-Esta clase trae por defecto la implementación del método POST
-'''
-class ProductList(ListCreateAPIView):
+
+class ProductList(ListAPIView):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
