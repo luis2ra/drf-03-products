6cda6b1ad1eee175924410e75f8fd1f81266069a
Luis Altuve <luis2ra@gmail.com>
2020-08-24 23:20:49 -0400

    Previa onfiguración de Authentication para aplicar en la API

--- drf_03_products/drf_03_products/settings.py
+++ drf_03_products/drf_03_products/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'rest_framework.authtoken',
     'catalog',
 ]
 
@@ -121,3 +122,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework.authentication.TokenAuthentication',
+    )
+}
