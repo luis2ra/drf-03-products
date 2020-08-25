43770f24bdfd7bf712ca283feb7fc65ce0737d16
Luis Altuve <luis2ra@gmail.com>
2020-08-24 23:16:02 -0400

    Ingreso de un registro usando el modelo de vista ListCreateAPIView

--- drf_03_products/catalog/views.py
+++ drf_03_products/catalog/views.py
@@ -1,9 +1,12 @@
-from rest_framework.generics import ListAPIView
+from rest_framework.generics import ListCreateAPIView
 from rest_framework.response import Response
 from .models import Product
 from .serializers import ProductSerializer
 
-
-class ProductList(ListAPIView):
+'''
+Probamos el uso de ListCreateAPIView en vez de ListAPIView
+Esta clase trae por defecto la implementación del método POST
+'''
+class ProductList(ListCreateAPIView):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
