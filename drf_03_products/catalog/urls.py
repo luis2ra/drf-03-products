98242f89d76f023c2d30d669940553de7536d2fa
Luis Altuve <luis2ra@gmail.com>
2020-08-24 23:10:40 -0400

    fase 2 de desarrollo de la API-Products. View basada en APIView

--- drf_03_products/catalog/urls.py
+++ drf_03_products/catalog/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 from . import views
+from catalog.views import ProductList
 
 urlpatterns = [
-    path('products/', views.product_list),
+    path('products/', ProductList.as_view()),
 ]
